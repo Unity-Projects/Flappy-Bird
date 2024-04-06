8cfeae9e76e498607b9f609c3d0feb7d4291ae2d
Luiz Coppini <coppiniluizcoppini@gmail.com>
2024-04-06 11:47:43 -0300

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+
+            if (Input.GetMouseButtonDown(0)) {
+
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        
+    }
+}
+
+}
